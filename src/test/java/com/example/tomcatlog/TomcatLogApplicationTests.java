1675f7d3c6dd4844d8107e16653bfab29be98a34
euideok.lee <sharplee@gmail.com>
2022-01-06 10:10:40 +0900

    first commit

---
+++ src/test/java/com/example/tomcatlog/TomcatLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomcatlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
